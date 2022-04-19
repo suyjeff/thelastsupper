88e949819de1bc31ee0ab1f342116f9e77609956
Jeffrey Su <sujeff@wustl.edu>
2022-04-19 16:39:39 -0500

    More progress

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-$(document).ready(function()
-{
+$(document).ready(function() {
+
   $('#menu-label').click(function() {
     if($(this).css("margin-right") == "240px")
     {
@@ -15,6 +15,29 @@ $(document).ready(function()
     $('#menu').animate({"margin-right": '-=240'});
   });
 
- });  
+  $("#about").on('click', function(){
+    window.location = "about.html";    
+  });
+
+  // $("#beef").hover(function() {
+  //   if (!$(this).hasClass("animated")) {
+  //     $(this).dequeue().stop().animate({ color: "#CE0000", content: "beef" });
+  //   }   
+  // }, function() {
+  //   $(this).addClass("animated").animate({ color: "#ffffff", content: "Amiodarone" }, "normal", "linear", function() {
+  //     $(this).removeClass("animated").dequeue();
+  //   });
+  // });
+
+  // $("#beef").hover(function() {
+  //   $(this).animate({ color: "#CE0000"});
+  // }, function() {
+  //   $(this).animate({ color: "#ffffff"});
+  // });
+  
+});
+  
+
+ 
 
 
